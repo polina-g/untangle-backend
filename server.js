6c7c25c22ab1e82c4181fc284e2dc50d549f1ab6
polina-g <ignatieva.polina@gmail.com>
2021-10-30 16:31:03 -0400

    modifications

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
-const { MONGODB_URL, PORT = 3001} = process.env;
+const { 
+    MONGODB_URL, 
+    PORT = 3001, 
+    PRIVATE_KEY_ID, 
+    PRIVATE_KEY, 
+    CLIENT_ID } = process.env;
 const admin = require('firebase-admin');
-const serviceAccount = require('./untangle-98fed-firebase-adminsdk-j2m7c-296acd69e7.json');
 
 const app = express(); 
 const entriesController = require('./controllers/entries');
@@ -32,14 +36,33 @@ app.use(express.static('public'))
 
 //Authorization Middleware
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "untangle-98fed",
+        "private_key_id": PRIVATE_KEY_ID,
+        "private_key": PRIVATE_KEY.replace(/\\n/g, '\n'),
+        "client_email": "firebase-adminsdk-j2m7c@untangle-98fed.iam.gserviceaccount.com",
+        "client_id": CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-j2m7c%40untangle-98fed.iam.gserviceaccount.com"
+      }
+    )
   });
 
 app.use(async function(req, res, next) {
     const token = req.get('Authorization');
-    const authUser = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
-    req.user = authUser;
-    next();
+    if (token) {
+        try {
+            const authUser = await admin.auth().verifyIdToken(token.replace('Bearer ', '')); 
+            req.user = authUser;  
+        } catch (error) {
+            console.log('catch error: ', error);
+            res.send('error on authorization')
+        }
+    } 
+    next();  
 })
 
 //router auth middleware function
